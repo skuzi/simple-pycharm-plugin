a719646a43b726e8f67760e855869e994fb33147
GitHub Action <action@github.com>
2020-09-15 13:58:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/skuzi/simplepycharmplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/skuzi/simplepycharmplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.skuzi.simplepycharmplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skuzi.simplepycharmplugin.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/skuzi/simplepycharmplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/skuzi/simplepycharmplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.skuzi.simplepycharmplugin.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.skuzi.simplepycharmplugin.MyBundle
 
 class MyProjectService(project: Project) {
 
