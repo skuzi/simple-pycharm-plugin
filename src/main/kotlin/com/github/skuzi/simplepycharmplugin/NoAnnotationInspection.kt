53bcebb87a0d35459b5b7dd5b5a5a09793e51639
skuzi <serg.kuzmin00@mail.ru>
2020-09-16 22:12:03 +0300

    add plugin functionality

---
+++ src/main/kotlin/com/github/skuzi/simplepycharmplugin/NoAnnotationInspection.kt
@@ -0,0 +1,42 @@
+package com.github.skuzi.simplepycharmplugin
+
+import com.github.skuzi.simplepycharmplugin.quickfix.AssignmentQuickFix
+import com.github.skuzi.simplepycharmplugin.quickfix.ParametersListQuickFix
+import com.intellij.codeInspection.ProblemsHolder
+import com.intellij.psi.PsiElement
+import com.jetbrains.python.inspections.PyInspection
+import com.jetbrains.python.psi.PyAssignmentStatement
+import com.jetbrains.python.psi.PyElementVisitor
+import com.jetbrains.python.psi.PyFunction
+
+/**
+ * Inspection to detect variable declarations without type annotations
+ */
+class NoAnnotationInspection : PyInspection() {
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): PyElementVisitor {
+        return object : PyElementVisitor() {
+
+            private val DESCRIPTION = "No type annotation"
+
+            override fun visitElement(element: PsiElement) {
+                super.visitElement(element)
+
+                if (element is PyAssignmentStatement && element.annotationValue == null) {
+                    holder.registerProblem(element, DESCRIPTION, AssignmentQuickFix())
+                    return
+                }
+
+                if (element is PyFunction) {
+                    if (element.parameterList.parameters.any { it.asNamed!!.annotationValue == null }) {
+                        holder.registerProblem(element.parameterList, DESCRIPTION, ParametersListQuickFix())
+                    }
+
+                    // TODO untyped function declaration
+
+                    return
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/github/skuzi/simplepycharmplugin/quickfix/ParametersListQuickFix.kt
@@ -0,0 +1,43 @@
+package com.github.skuzi.simplepycharmplugin.quickfix
+
+import com.intellij.codeInspection.LocalQuickFix
+import com.intellij.codeInspection.ProblemDescriptor
+import com.intellij.openapi.project.Project
+import com.jetbrains.python.psi.LanguageLevel
+import com.jetbrains.python.psi.PyElementGenerator
+import com.jetbrains.python.psi.PyParameterList
+
+/**
+ * Quick fix for functions where at least one parameter is not type annotated
+ */
+class ParametersListQuickFix : LocalQuickFix {
+    override fun getFamilyName(): String {
+        return name
+    }
+
+    override fun getName(): String {
+        return "Annotate function parameters"
+    }
+
+    override fun applyFix(project: Project, descriptor: ProblemDescriptor) {
+        val parameterList = descriptor.psiElement
+        if (parameterList !is PyParameterList) {
+            return
+        }
+
+        val generator = PyElementGenerator.getInstance(project)
+        val newParameterList = generator.createParameterList(LanguageLevel.PYTHON38, "()")
+        parameterList.parameters.forEach {
+            newParameterList.addParameter(
+                generator.createParameter(
+                    it.name!!,
+                    it.defaultValueText,
+                    "int",
+                    LanguageLevel.PYTHON38
+                )
+            )
+        }
+
+        parameterList.replace(newParameterList)
+    }
+}
